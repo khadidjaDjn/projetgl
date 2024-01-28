09fe689b8045070810e9f1328114042a4787ebef
Hani Gaouaou <104110500+hanyG175@users.noreply.github.com>
2024-01-29 00:43:46 +0100

    Revert "Backend"

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import GoogleLoginApi
-
-
-urlpatterns = [
-      path("auth/login/google/", GoogleLoginApi.as_view(), 
-         name="login-with-google"),
-]
