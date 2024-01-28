b41e2701b9f67a6ae0e14ff8d31ab292fa435c9f
khadidjaDjn <121129321+khadidjaDjn@users.noreply.github.com>
2024-01-28 18:45:48 +0100

    v

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import './HomePage.css'
 
    const HomePage = () => {
 
     return ( 
         <>
+           <div className="homePage">
            <Navbar />
            <Home/>
+           </div>
            
         </>
 
